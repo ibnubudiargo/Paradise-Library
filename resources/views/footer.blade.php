72a0b57e9cac72ff8d035c47876704068dbe3bc9
ibnubudiargo <ibnubudiargo123@gmail.com>
2023-10-24 23:32:45 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<p>
+    <!--awal FOOTER-->
+<div class="col-md-12" style="padding-top: 10px;">
+    <div class="row" style="background: #A6F6FF;">
+        <div class="col-md-9">
+            <h3>Visi</h3>
+            <p>Terwujudnya Indonesia Cerdas Melalui Gemar Membaca Dengan Memberdayakan Perpustakaan
+
+            <p>
+            <h3>Misi</h3>
+            <ul>
+                <li>Terwujudnya layanan prima</li>
+                <li>Terwujudnya perpustakaan sebagai pelestari khazanah budaya bangsa</li>
+                <li>Terwujudnya perpustakaan sesuai standar nasional perpustakaan</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
+            Copyright &copy; 2023. All right reserve
+            <p>
+                Developed by Ibnu budiargo
+        </div>
+    </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#A6F6FF" fill-opacity="1" d="M0,128L21.8,154.7C43.6,181,87,235,131,240C174.5,245,218,203,262,202.7C305.5,203,349,245,393,245.3C436.4,245,480,203,524,202.7C567.3,203,611,245,655,224C698.2,203,742,117,785,80C829.1,43,873,53,916,69.3C960,85,1004,107,1047,122.7C1090.9,139,1135,149,1178,176C1221.8,203,1265,245,1309,224C1352.7,203,1396,117,1418,74.7L1440,32L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path>
+</svg>
+<!--akhir FOOTER-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
---
+++ resources/views/view_home.blade.php
@@ -0,0 +1,16 @@
+@extends('index')
+@section('title', 'Home')
+
+@section('isihalaman')
+<h3>Tujuan Utama Paradise library</h3>
+<p>
+<p>Memberikan Akses ke Informasi: Perpustakaan bertujuan untuk memberikan akses yang mudah dan efisien kepada berbagai jenis informasi, termasuk buku, jurnal, majalah, surat kabar, e-book, rekaman audio, video, dan sumber informasi lainnya.</p>
+<p>Pendidikan: Perpustakaan mendukung pendidikan dengan menyediakan bahan bacaan dan sumber daya yang diperlukan untuk pembelajaran, penelitian, dan pengembangan pribadi.</p>
+<p>Penelitian: Perpustakaan adalah tempat yang penting bagi peneliti dan akademisi untuk mengakses literatur ilmiah dan sumber daya penelitian lainnya. Tujuannya adalah untuk mendukung penelitian dan penemuan ilmiah.</p>
+<p>Budaya dan Hiburan: Perpustakaan juga memiliki peran dalam mempromosikan budaya dan hiburan dengan menyediakan koleksi fiksi, karya seni, musik, dan hiburan lainnya.</p>
+<p>Pengembangan Komunitas: Perpustakaan sering menjadi pusat kegiatan komunitas, menyelenggarakan berbagai program, seperti klub buku, lokakarya, pertemuan penulis, dan acara budaya lainnya untuk memperkaya kehidupan komunitas.</p>
+<p>Literasi Informasi: Perpustakaan berkontribusi pada peningkatan literasi informasi, membantu pengguna memahami cara mencari, mengevaluasi, dan menggunakan sumber informasi dengan efektif.</p>
+<p>Pelestarian Warisan Budaya: Perpustakaan juga bertujuan untuk melestarikan warisan budaya dengan mengoleksi dan merawat bahan bersejarah dan budaya, seperti manuskrip kuno, arsip, dan dokumen penting lainnya.</p>
+<p>Ketersediaan Teknologi: Banyak perpustakaan modern juga bertujuan untuk menyediakan akses ke teknologi, termasuk komputer, internet, dan sumber daya digital.</p>
+<p>
+    @endsection
